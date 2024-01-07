d523e2cc0b0bfcd8867cef4a073e135de5f3d9a4
Ilya <azazafkz@gmail.com>
2024-01-07 16:22:12 +0300

    WebSocket mess

--- BackendNodeJS/index.js
+++ BackendNodeJS/index.js
@@ -1,12 +1,14 @@
 var express = require('express');
+var ws = require("./ws.js");
 
 var app = express();
-app.get('/', function(req, res) {
-        res.send('Hello World! Test commit. Popytka nomer dva!');
-    });
+app.get('/', function (req, res) {
+    res.send('Hello World! Test commit. Popytka nomer dva!');
+});
 
-var server = app.listen(3000, function(){
+var server = app.listen(8080, function () {
     var host = server.address().address;
     var port = server.address().port;
-    console.log("Example app listening at http://%s:%s", host, port);
+    console.log("Сервер работает на порту 8080");
+    ws.ws();
 });
--- Frontend/reactapp/src/pages/MessengerPage.tsx
+++ Frontend/reactapp/src/pages/MessengerPage.tsx
@@ -1,33 +1,88 @@
-import React, {useState} from 'react';
-import {observer} from "mobx-react-lite";
+import React, { useState } from 'react';
+import { observer } from "mobx-react-lite";
 
 const MessengerPage = observer(() => {
+    //WebSocket
+    const myWs = new WebSocket('ws://localhost:9000');
+    myWs.onopen = function () {
+        console.log('подключился');
+    };
+    myWs.onmessage = function (message) {
+        console.log('Message: %s', message.data);
+    };
+    function wsSendEcho() {
+        let text = document.getElementById("mess")?.textContent;
+        myWs.send(JSON.stringify({ action: 'ECHO', data: text }));
+    };
+    function wsSendPing() {
+        myWs.send(JSON.stringify({ action: 'PING' }));
+    };
+
     const id = 1234
-    const [dialogMessagges, setDialogMessagese] = useState([{id: 1,sender: 1234, text: "texs12w", date: ""},
-        {id: 2,sender: 1234, text: "texs12w", date: ""},
-        {id: 3,sender: 1234512, text: "texs12w", date: ""},
-        {id: 4,sender: 1234, text: "texs12w", date: ""},
-        {id: 5,sender: 1234512, text: "texs12w", date: ""},
-        {id: 6,sender: 1234512, text: "texs12w", date: ""},
-        {id: 7,sender: 1234, text: "texs12w", date: ""}])
+    const [dialogMessagges, setDialogMessagese] = useState([{ id: 1, sender: 1234, text: "texs12w", date: "" },
+    { id: 2, sender: 1234, text: "texs12w", date: "" },
+    { id: 3, sender: 1234512, text: "texs12w", date: "" },
+    { id: 4, sender: 1234, text: "texs12w", date: "" },
+    { id: 5, sender: 1234512, text: "texs12w", date: "" },
+    { id: 6, sender: 1234512, text: "texs12w", date: "" },
+    { id: 7, sender: 1234, text: "texs12w", date: "" }])
     return (
-        <div style={{display:"flex"}}>
-            <div style={{display: 'flex', background: "honeydew", maxWidth: "900px", width: "700px", minHeight: "500px", flexDirection: "column"}}>
-                <div style={{display: "flex", flexDirection: "column-reverse", width: "100%", height: "100%"}}>
+        <div style={{ display: "flex" }}>
+            <div style={{ display: 'flex', background: "honeydew", maxWidth: "900px", width: "700px", minHeight: "500px", flexDirection: "column" }}>
+                <div style={{ display: "flex", flexDirection: "column-reverse", width: "100%", height: "100%" }}>
                     {
                         dialogMessagges.map(message =>
-                                <div  key={message.id} style={id === message.sender ? {background: "lightblue", marginRight:"35px", marginLeft: "auto", marginTop: "5px", marginBottom:"5px"}: {background: "lightgreen", marginRight:"auto", marginLeft: "35px", marginTop: "5px", marginBottom:"5px"}}>{message.text}</div>
-                            )
+                            <div key={message.id} style={id === message.sender ? {
+                                background: "lightblue",
+                                marginRight: "35px",
+                                marginLeft: "auto",
+                                marginTop: "5px",
+                                marginBottom: "5px"
+                            } : {
+                                background: "lightgreen",
+                                marginRight: "auto",
+                                marginLeft: "35px",
+                                marginTop: "5px",
+                                marginBottom: "5px"
+                            }}>{message.text}</div>
+                        )
                     }
                 </div>
-                <div style={{display: "flex", marginBottom: "0px", marginTop: "auto", background: "yellowgreen", width: "100%", maxHeight: "120px", paddingTop: "", alignItems: "center"}}>
-                    <div style={{marginBottom: "0px", marginTop: "auto", width: "30px", height: "30px",  marginLeft: "auto"}}>X</div>
-                    <div contentEditable={"true"} style={{padding: "3px", borderRadius: "5px", background: "yellow", width: "450px", minHeight: "30px",maxHeight: "120px", resize: "none", marginTop: "5px", marginBottom:"5px", overflowX: "hidden", overflowY: "auto", textAlign: "start", boxSizing: "border-box"}}></div>
-                    <input type={"button"} value={"X"} style={{marginRight: "auto", marginLeft: "5px", width: "25px", height: "25px",}}/>
+                <div style={{
+                    display: "flex",
+                    marginBottom: "0px",
+                    marginTop: "auto",
+                    background: "yellowgreen",
+                    width: "100%",
+                    maxHeight: "120px",
+                    paddingTop: "",
+                    alignItems: "center"
+                }}>
+                    <div style={{
+                        marginBottom: "0px",
+                        marginTop: "auto",
+                        width: "30px",
+                        height: "30px",
+                        marginLeft: "auto"
+                    }}>X</div>
+                    <div id='mess' contentEditable={"true"} style={{
+                        padding: "3px",
+                        borderRadius: "5px",
+                        background: "yellow",
+                        width: "450px",
+                        minHeight: "30px",
+                        maxHeight: "120px",
+                        resize: "none",
+                        marginTop: "5px",
+                        marginBottom: "5px",
+                        overflowX: "hidden",
+                        overflowY: "auto",
+                        textAlign: "start",
+                        boxSizing: "border-box"
+                    }}></div>
+                    <input type={"button"} onClick={wsSendEcho} value={"X"} style={{ marginRight: "auto", marginLeft: "5px", width: "25px", height: "25px", }} />
                 </div>
             </div>
-
-
         </div>
     );
 });
