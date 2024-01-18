ce6ca0ef07abaef17b1534db6cf30540446b6c42
fbrtyu <azazafkz@gmail.com>
2024-01-18 21:54:20 +0300

    Backend API for chat and messages
    SOCIALNETWORK-8
    Add app.get('/getchatmessages')
    Add app.get('/getchats')

--- BackendNodeJS/index.js
+++ BackendNodeJS/index.js
@@ -8,15 +8,8 @@ var app = express();
 app.get('/getmessages', async function (req, res) {
     // Website you wish to allow to connect
     res.setHeader('Access-Control-Allow-Origin', '*');
-
-    // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
     res.setHeader('Access-Control-Allow-Credentials', true);
 
     let groupid = await req.query.groupid;
@@ -25,6 +18,34 @@ app.get('/getmessages', async function (req, res) {
 
     res.send(JSON.stringify(answer));
 });
+
+app.get('/getchats', async function(req, res) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    
+    let userid = await req.query.userid;
+
+    let answer = await pg.getchats(userid);
+
+    res.send(JSON.stringify(answer));
+})
+
+app.get('/getchatmessages', async function(req, res) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    
+    let userid = await req.query.userid;
+
+    let groupid = await req.query.groupid;
+
+    let answer = await pg.getonechatmessages(userid, groupid);
+
+    res.send(JSON.stringify(answer));
+})
 
 var server = app.listen(8080, function () {
     var host = server.address().address;
--- BackendNodeJS/pg.js
+++ BackendNodeJS/pg.js
@@ -2,10 +2,30 @@ const { Client } = require("pg")
 const dotenv = require("dotenv")
 dotenv.config()
 
+//Класс для ответа getchats
+class getchatsAnswer {
+    Count = 0;
+    Chats = [];
+
+    constructor(count, chats) {
+        this.Count = count;
+        this.Chats = chats;
+    }
+}
+
+//Класс для ответа getmessages
+class getchatMessagesAnswer {
+    ChatId = 0;
+    LastMessages = [];
+
+    constructor(groupid, chatmessages) {
+        this.ChatId = groupid;
+        this.LastMessages = chatmessages;
+    }
+}
 
 //Пока просто тестовая функция для выполнения INSERT в БД
 const pgsendmessage = async (data) => {
-
     try {
         const client = new Client({
             user: process.env.PGUSER,
@@ -54,6 +74,90 @@ const pggetmessage = async (groupid) => {
         console.log(error)
     }
 }
+
+//Функция получения всех чатов пользователя из БД по userid
+const getchats = async (userid) => {
+    try {
+        const client = new Client({
+            user: process.env.PGUSER,
+            host: process.env.PGHOST,
+            database: process.env.PGDATABASE,
+            password: process.env.PGPASSWORD,
+            port: process.env.PGPORT
+        })
+
+        await client.connect()
+
+        const query = {
+            text: 'SELECT groupid, usercreaterid, userid, name FROM chat INNER JOIN chatuserinfo ON (chat.id = chatuserinfo.groupid) WHERE userid = $1',
+            values: [userid],
+        }
+
+        const res = await client.query(query)
+        let chats = res.rows;
+        let rc = res.rowCount;
+
+        let chat = [];
+
+        while (rc - 1 >= 0) {
+
+            const query2 = {
+                text: 'SELECT userid, firstname FROM userinfo INNER JOIN (SELECT groupid, userid FROM chat INNER JOIN chatuserinfo ON (chat.id = chatuserinfo.groupid) WHERE groupid = $1) ON (userinfo.id = userid)',
+                values: [res.rows[rc - 1].groupid],
+            }
+    
+            const res2 = await client.query(query2)
+            let chatusers = res2.rows
+
+            objectT = JSON.parse(JSON.stringify(res.rows[rc - 1]));
+            objectT['Users'] = chatusers;
+            chat.push(objectT);
+
+            rc = rc - 1;
+        }
+
+        await client.end()
+
+        const answer = new getchatsAnswer(chats.length, chat)
+        return answer
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+//Функция получения всех сообщений чата пользователя из БД по userid и groupid
+const getonechatmessages = async (userid, groupid) => {
+    try {
+        const client = new Client({
+            user: process.env.PGUSER,
+            host: process.env.PGHOST,
+            database: process.env.PGDATABASE,
+            password: process.env.PGPASSWORD,
+            port: process.env.PGPORT
+        })
+
+        await client.connect()
+
+        const query = {
+            text: 'SELECT groupid, message.id, usersenderid, firstname, text, createdate, updatedate FROM userinfo INNER JOIN message ON (userinfo.id = message.usersenderid) WHERE groupid = $1',
+            values: [groupid],
+        }
+
+        const res = await client.query(query)
+        let chatmessages = res.rows;
+
+        await client.end()
+
+        const answer = new getchatMessagesAnswer(groupid, chatmessages)
+        return answer
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+module.exports.getonechatmessages = getonechatmessages;
+
+module.exports.getchats = getchats;
 
 module.exports.pgsendmessage = pgsendmessage;
 
